e483caba15519f420dfd948a18eb615e3292cceb
Kirk Gamo <104485268+KXAZTHRA@users.noreply.github.com>
2023-12-02 14:36:31 +0000

    add final project

---
+++  final_project/fitness.cpp
@@ -0,0 +1,29 @@
+/*Final Proj 
+Assume that your user is a fitness/wellness enthusiast, write a program that
+(1) has function computing the current BMI
+(2) has a function that computes daily calorie intake based on its food choices (you have to have a predefined data food intake)
+(3) has a function that gives the non-workout/daily activities calories burn 
+(4)if doing workout, the type of workout with various calorie burn value based on number of repetitions and sets
+(5) shall be able to predict when to attain the desired weight based on date started and ended
+(6)provide data  flow dialogue
+(7)provide pseudocode
+(8)provide (???) for data sets*/
+
+// Declare data set for food name, and its corresponding value
+
+// Declare data set for work-out activities, and its corresponding calorie burn value
+
+int main() {
+    // Display message about how to use the program
+
+    // Ask and Calculate BMI
+
+    // Ask user for foods eaten a day and compute the overall calorie intake
+
+    // Ask user for non-workout activities that may burn calories
+
+    // Ask user for the type of workout with equivalent calorie burn values based on the number of repetition anf sets
+
+    // Ask user for his/her desired weight, date the workout started and compute when will the user attain that desired weight
+
+}
